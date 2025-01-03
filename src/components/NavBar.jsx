d25d05ce9ec6cbfa87b9083dc9fd5353c53d9c6b
Jintops <jintops2003@gmail.com>
2025-01-03 15:13:06 +0530

    logout user

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom';
+import { removeUser } from '../utils/userSlice';
+import axios from 'axios';
+import { BASE_URL } from '../utils/constants';
 
 const NavBar = () => {
 
   const user=useSelector(store=>store.user);
+  const dispatch=useDispatch();
+ const navigate=useNavigate();
+
+  const handleLogOut=async()=>{
+    try{
+      await axios.post(BASE_URL+"/logout",{},{withCredentials:true}) 
+     dispatch(removeUser());
+     return navigate("/login")
+    }catch(err){
+      console.error(err)
+    }
+   
+  }
  
   return (
     <div className="navbar bg-base-300">
@@ -35,7 +51,7 @@ const NavBar = () => {
             </Link>
           </li>
           <li><a>Settings</a></li>
-          <li><a>Logout</a></li>
+          <li><a onClick={handleLogOut}>Logout</a></li>
         </ul>
       </div>
     </div>}
