d3f203c409ededd2b294812a477985c268c8d203
Jintops <jintops2003@gmail.com>
2025-01-16 00:06:23 +0530

    changing the routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,37 @@
-import { BrowserRouter, Routes,Route } from "react-router-dom"
-import Login from "./components/Login"
-import Body from "./components/Body"
-import Profile from "./components/Profile"
-import { Provider } from "react-redux"
-import appStore from "./utils/appStore"
-import Feed from "./components/Feed"
-import Connections from "./components/Connections"
-import Requests from "./components/Requests"
-import HomePage from "./components/HomePage"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import Feed from "./components/Feed";
+import Connections from "./components/Connections";
+import Requests from "./components/Requests";
+import HomePage from "./components/HomePage";
+import Footer from "./components/Footer";  // Import Footer
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
+import Body from "./components/Body";  // Ensure Body is a proper layout
 
 function App() {
- 
-
   return (
-    <>
     <Provider store={appStore}>
-    <BrowserRouter basename="/">
-    <Routes>
-      <Route path="/" element={<Body/>}>
-      <Route path="/" element={<HomePage/>}></Route>
-      <Route path="/feed" element={<Feed/>}></Route>
-      <Route path="/profile" element={<Profile/>}></Route>
-      <Route path="/login" element={<Login/>}></Route>
-      <Route path="/connections" element={<Connections/>}></Route>
-      <Route path="/requests" element={<Requests/>}></Route>
-      </Route>
-      
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <div className="flex flex-col min-h-screen">
+          <div className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Body />}>
+                <Route index element={<HomePage />} />
+                <Route path="feed" element={<Feed />} />
+                <Route path="profile" element={<Profile />} />
+                <Route path="login" element={<Login />} />
+                <Route path="connections" element={<Connections />} />
+                <Route path="requests" element={<Requests />} />
+              </Route>
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      </BrowserRouter>
     </Provider>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
