d7f279ea59a754627e842176fa81b1622b01f1dd
Jintops <jintops2003@gmail.com>
2025-01-16 23:03:11 +0530

    login & signup design

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
       }, {
         withCredentials: true,
       })
-     console.log(res.data)
+      console.log(res.data)
       dispatch(addUser(res.data))
       return navigate("/feed")
     } catch (err) {
@@ -45,46 +45,64 @@ const Login = () => {
 
   return (
     <div className='flex justify-center my-14'>
-      <div className="card bg-base-300 w-96 shadow-xl ">
+      <div className="card bg-base-200 w-96 shadow-xl ">
         <div className="card-body">
-          <h2 className="card-title justify-center">{isLogin ? "Login" : "SignUp"} </h2>
+          <h2 className="card-title justify-center text-2xl font-bold text-white">{isLogin ? "Login" : "Create Account"} </h2>
 
-          {!isLogin && <><label className="form-control w-full max-w-xs">
-            <div className="label">
-              <span className="label-text">First Name</span>
+          {!isLogin && <><label className="form-control w-full max-w-xs mt-6">
+            <div className="label" >
+              <span className="label-text text-white">First Name</span>
             </div>
-            <input type="text" value={firstName} className="input input-bordered w-full max-w-xs" onChange={(e) => setFirstName(e.target.value)} />
+            <input type="text" value={firstName} className="input input-bordered w-full max-w-xs text-white" placeholder='Enter your first name' onChange={(e) => setFirstName(e.target.value)} />
           </label>
-
-            <label className="form-control w-full max-w-xs">
+          </>}
+          {/* <label className="form-control w-full max-w-xs">
               <div className="label">
-                <span className="label-text">Last Name</span>
+                <span className="label-text text-white">Last Name</span>
               </div>
               <input type="text" value={lastName} className="input input-bordered w-full max-w-xs" onChange={(e) => setLastname(e.target.value)} />
-            </label></>}
+            </label> */}
 
 
-          <label className="form-control w-full max-w-xs">
+          <label className="form-control w-full max-w-xs mt-4 my-4">
             <div className="label">
-              <span className="label-text">Email ID</span>
+              <span className="label-text text-white">Email ID</span>
             </div>
-            <input type="text" value={emailId} className="input input-bordered w-full max-w-xs" onChange={(e) => setEmailId(e.target.value)} />
+            <input type="text" value={emailId} className="input input-bordered w-full max-w-xs text-white" placeholder='Enter your email'  onChange={(e) => setEmailId(e.target.value)} />
           </label>
 
           <label className="form-control w-full max-w-xs">
             <div className="label">
-              <span className="label-text">Password</span>
+              <span className="label-text text-white">Password</span>
             </div>
-            <input type="text" value={password} className="input input-bordered w-full max-w-xs" onChange={(e) => setPassword(e.target.value)} />
+            <input type="text" value={password} className="input input-bordered w-full max-w-xs text-white" placeholder='Enter your password' onChange={(e) => setPassword(e.target.value)} />
           </label>
           <p className='text-red-500'>{error}</p>
 
           <div className="card-actions justify-center my-2">
-            <button className="btn btn-primary" onClick={isLogin ? handleLogin : handleSignUp}>{isLogin ? "Login" : "SignUp "}</button>
+            <button className="btn bg-pink-500 w-full max-w-xs text-white hover:bg-pink-600" onClick={isLogin ? handleLogin : handleSignUp}>{isLogin ? "Login" : "SignUp "}</button>
           </div>
-          <div className='my-5'>
-            <p className='cursor-pointer text-center mx-5' onClick={() => setIsLogin((value) => !value)}>{isLogin ? "New to devTinder? Create an account" : "Existing User? Log in"}</p>
+
+          <div className="">
+            <p className="cursor-pointer text-start text-sm  text-white" onClick={() => setIsLogin((value) => !value)}>
+              {isLogin ? (
+                <>
+                  Don't have an account?{" "}
+                  <span className="text-pink-400 hover:text-pink-500 font-semibold">
+                    Register here
+                  </span>
+                </>
+              ) : (
+                <>
+                  You have an account?{" "}
+                  <span className="text-pink-400 hover:text-blue-500 font-semibold">
+                     Login here
+                  </span>
+                </>
+              )}
+            </p>
           </div>
+
         </div>
       </div>
     </div>
