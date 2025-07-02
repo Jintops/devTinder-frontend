d9284ffefee00ccffa1902f6da4425198bce772f
Jintops <jintops2003@gmail.com>
2025-07-03 04:06:22 +0530

    adding previous msg also

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -20,7 +20,7 @@ const Chat = () => {
 
     socket.on("messageReceiver",({firstName,text})=>{
         console.log(firstName+":"+text)
-        setMessages([...messages,{firstName,text}])
+        setMessages((messages)=>[...messages,{firstName,text}])
     })
 
     return ()=>{
@@ -32,36 +32,46 @@ const Chat = () => {
     const sendMessage=()=>{
          const socket=createSocketConnection();
         socket.emit("sendMessage",{firstName:user.firstName,targetUserId,userId,text:newMessage})
+        setNewMessage("")
     }
 
   return (
     <div className='w-1/2 items-center border border-white justify-center m-auto'>
         <h1 className='border text-white font-bold p-5'>chat</h1>
-        <div className='h-72'>
-    <div className="chat chat-start">
-  <div className="chat-image avatar">
-    <div className="w-10 rounded-full">
-      <img
-        alt="Tailwind CSS chat bubble component"
-        src="https://img.daisyui.com/images/profile/demo/kenobee@192.webp"
-      />
+        <div className='h-72 overflow-scroll'>
+    
+  
+ {messages.map((msg, index) => {
+  return (
+    <div key={index} className="chat chat-start mb-2 mt-1">
+      <div className="chat-image avatar">
+        <div className="w-10 rounded-full">
+          <img
+            alt="User Avatar"
+            src="https://img.daisyui.com/images/profile/demo/kenobee@192.webp"
+          />
+        </div>
+      </div>
+      <div className="chat-bubble bg-blue-100 text-black">
+        <div className="flex justify-between items-center mb-1 text-xs font-semibold text-gray-600">
+          <span>{msg.firstName}</span>
+          <span className="opacity-50">12:45</span>
+        </div>
+        <div className="text-sm mb-1">
+          {msg.text}
+        </div>
+        <div className="flex justify-between text-xs text-gray-500 mt-1">
+          <span>seen</span>
+          <span>✓✓</span> 
+        </div>
+      </div>
     </div>
-  </div>
-  {messages.map((msg,index)=>{
-    return(
-        <>
-         <div key={index} className="chat-header">
-    {msg.firstName}
-    <time className="text-xs opacity-50">12:45</time>
-  </div>
-  <div className="chat-bubble">{msg.text}</div>
-  <div className="chat-footer opacity-50">seen</div>
-  </>
+  );
+})}
 
-    )
-  })}
+
  
-</div>
+
 <div className="chat chat-end ">
   <div className="chat-image avatar">
     <div className="w-10 rounded-full">
