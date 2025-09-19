60c85ac2faac194ff1842ae8be820c43539dda17
Jintops <jintops2003@gmail.com>
2025-09-20 04:16:34 +0530

    updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -41,20 +41,29 @@ const Header = () => {
                         </p>
 
                         <div className="dropdown dropdown-end">
-                            <div
-                                tabIndex={0}
-                                role="button"
-                                className="btn btn-ghost btn-circle avatar">
-                                <div className="w-10 rounded-full">
-                                    <img
-                                        alt="Tailwind CSS Navbar component"
-                                        src={user.photoUrl}
-                                    />
-                                </div>
-                            </div>
+                            <div 
+    tabIndex={0} 
+    role="button" 
+    className="btn btn-ghost flex items-center gap-2 px-2"
+  >
+    <div className="w-10 rounded-full overflow-hidden">
+      <img alt="User avatar" src={user.photoUrl} />
+    </div>
+    {/* ▼ Arrow with transition */}
+    <svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      className="h-4 w-4 text-white  transition-transform duration-200 dropdown-arrow"
+      fill="none" 
+      viewBox="0 0 24 24" 
+      stroke="currentColor" 
+      strokeWidth={2}
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
+    </svg>
+  </div>
                             <ul
                                 tabIndex={0}
-                                className="menu menu-base dropdown-content bg-base-100 rounded-box z-1 w-40 shadow p-0 py-2">
+                                className="menu menu-base dropdown-content bg-base-100 rounded-box z-[1000] w-40 shadow p-0 py-2">
                                 <p className="font-bold px-3">My Account</p>
                                 <div className="divider m-0 mt-1"></div>
                                 <div className="space-y-1">
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,76 +1,77 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Link, useNavigate } from 'react-router-dom';
-import { removeUser } from '../utils/userSlice';
-import axios from 'axios';
-import { BASE_URL } from '../utils/constants';
-import { FaCode } from "react-icons/fa";
+// import React from 'react'
+// import { useDispatch, useSelector } from 'react-redux'
+// import { Link, useNavigate } from 'react-router-dom';
+// import { removeUser } from '../utils/userSlice';
+// import axios from 'axios';
+// import { BASE_URL } from '../utils/constants';
+// import { FaCode } from "react-icons/fa";
 
-const NavBar = () => {
+// const NavBar = () => {
 
-  const user = useSelector(store => store.user);
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+//   const user = useSelector(store => store.user);
+//   const dispatch = useDispatch();
+//   const navigate = useNavigate();
  
-   const handleLogin=()=>{
-    navigate('/login')
-   }
-  const handleLogOut = async () => {
-    try {
-      await axios.post(BASE_URL + "/logout", {}, { withCredentials: true })
-      dispatch(removeUser());
-      return navigate("/")
-    } catch (err) {
-      console.error(err)
-    }
-  }
+//    const handleLogin=()=>{
+//     navigate('/login')
+//    }
+//   const handleLogOut = async () => {
+//     try {
+//       await axios.post(BASE_URL + "/logout", {}, { withCredentials: true })
+//       dispatch(removeUser());
+//       return navigate("/")
+//     } catch (err) {
+//       console.error(err)
+//     }
+//   }
 
-  return (
-    <div className="navbar bg-base-300">
-         <div className="flex-1">
-                    <Link
-                        to="/"
-                        className="flex items-center gap-1 sm:gap-2 font-bold text-lg sm:text-xl">
-                        <FaCode className="w-5 h-5 sm:h-6 sm:w-6" />
-                        <span>DevTinder</span>
-                    </Link>
-                </div>
-      <div>
-     {!user && <button className=" btn  bg-slate-800 hover:bg-gray-700 text-white" onClick={handleLogin}>Login</button>}
-      </div>
+//   return (
+//     <div className="navbar bg-base-300">
+//          <div className="flex-1">
+//                     <Link
+//                         to="/"
+//                         className="flex items-center gap-1 sm:gap-2 font-bold text-lg sm:text-xl">
+//                         <FaCode className="w-5 h-5 sm:h-6 sm:w-6" />
+//                         <span>DevTinder</span>
+//                     </Link>
+//                 </div>
+//       <div>
+//      {!user && <button className=" btn  bg-slate-800 hover:bg-gray-700 text-white" onClick={handleLogin}>Login</button>}
+//       </div>
       
-      {user && <div className=''>
-        <p className='font-bold'> Welcome, {user.firstName}</p>
-      </div>}
-      {user && <div className="flex-none gap-2">
+//       {user && <div className=''>
+//         <p className='font-bold'> Welcome, {user.firstName}</p>
+//       </div>}
+//       {user && <div className="flex-none gap-2">
 
-        <div className="dropdown dropdown-end mx-4 ">
-          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar bg-pink-800 hover:bg-pink-900">
-            <div className="w-10 rounded-full ">
-              <img
-                alt="photo of user"
-                src={user.photoUrl} />
-            </div>
-          </div>
-          <ul 
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-            <li>
-              <Link to="/profile" className="justify-between ">
-                Profile
+//         <div className="dropdown dropdown-end mx-4 ">
+//           <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar bg-pink-800 hover:bg-pink-900">
+//             <div className="w-10 rounded-full ">
+//               <img
+//                 alt="photo of user"
+//                 src={user.photoUrl} />
+//             </div>
+            
+//           </div>
+//           <ul 
+//             tabIndex={0}
+//             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+//             <li>
+//               <Link to="/profile" className="justify-between ">
+//                 Profile
               
-              </Link>
-            </li>
-            <li><Link to="/connections">Connections</Link></li>
-            <li><Link to="/feed">Feed</Link></li>
-            <li><Link to="/requests">Requests</Link></li>
-             <li><Link to="/premium">Premium</Link></li>
-            <li><a onClick={handleLogOut}>Log out</a></li>
-          </ul>
-        </div>
-      </div>}
-    </div>
-  )
-}
+//               </Link>
+//             </li>
+//             <li><Link to="/connections">Connections</Link></li>
+//             <li><Link to="/feed">Feed</Link></li>
+//             <li><Link to="/requests">Requests</Link></li>
+//              <li><Link to="/premium">Premium</Link></li>
+//             <li><a onClick={handleLogOut}>Log out</a></li>
+//           </ul>
+//         </div>
+//       </div>}
+//     </div>
+//   )
+// }
 
-export default NavBar
+// export default NavBar
