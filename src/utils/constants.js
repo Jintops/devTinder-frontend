11d403df59cb7015ba64a69afebfb0adfecaa1bf
Jintops <jintops2003@gmail.com>
2025-09-20 04:37:42 +0530

    change

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,1 @@
-export const BASE_URL=  location.hostname === "localhost" ? "http://localhost:7777" : "https://devtinder-20ec.onrender.com/api";
+export const BASE_URL=  location.hostname === "localhost" ? "http://localhost:7777" : "https://devtinder-20ec.onrender.com";
